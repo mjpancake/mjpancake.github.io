ba16a6a955b087455d5f62c737c931af3982405b
rolevax <rolevax@gmail.com>
2017-10-14 11:04:00 +0800

    change ajax srv addr

--- js/teru.js
+++ js/teru.js
@@ -1,9 +1,9 @@
 var teru = {};
 
 teru.send = function(method, api, msg, onSuccess) {
-	var ip = "118.89.219.207";
-	//var ip = "127.0.0.1";
-    var url = "http://" + ip + ":8080" + api;
+	var domain = "rolevax.xyz";
+	//var domain = "127.0.0.1";
+    var url = "https://" + domain + ":8080" + api;
 
     xmlhttp = new XMLHttpRequest();
     xmlhttp.open(method, url, true);
